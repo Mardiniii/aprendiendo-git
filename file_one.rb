85cf4685b2344cd6baf8740a8a4facc2b4c4e4f0
mardiniii <sezama03@hotmail.com>
2016-09-24 10:07:39 -0500

    Get two numbers from user

--- file_one.rb
+++ file_one.rb
@@ -1,3 +1,12 @@
+breakline = "\n"
 puts "Hi we are learning Git!"
 
 puts "This is my second line!"
+
+print "Number 1: "
+number_one = gets.chomp
+puts breakline
+
+print "Number 2: "
+number_two = gets.chomp
+puts breakline
